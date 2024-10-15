f07e84fa8442e94f2b05148d9a495fa2a0acd294
watching-dev <coderefactoring90@gmail.com>
2024-10-15 16:48:47 +0900

    [feat] leftSection Navigation 추가

--- src/app/(beforeLogin)/_components/homeSection/homeLeftSection.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeLeftSection.tsx
@@ -3,7 +3,18 @@ import styles from "./homeLeftSection.module.css";
 export default function HomeLeftSection() {
   return (
     <div className={styles.leftSectionWrapper}>
-      <div className={styles.leftSection}></div>
+      <div className={styles.leftSection}>
+        <div className={styles.fixed}>
+          <div className={styles.logo}>로고</div>
+          <nav>
+            <ul>
+              <li>홈</li>
+              <li>프로필</li>
+              <li>탐색하기</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
