57baab15dc56258ed40d80f219428456d11396ac
watching-dev <coderefactoring90@gmail.com>
2024-10-18 17:40:08 +0900

    [feat] Main Tab 추가

--- src/app/(beforeLogin)/_components/homeSection/homeMainSection/RightSectionMain.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeMainSection/RightSectionMain.tsx
@@ -1,7 +1,13 @@
 import styles from "./rightSectionMain.module.css";
+import Tab from "./tab/Tab";
 import TabProvider from "./tab/TabProvider";
 
 export default function RightSectionMain() {
-  return <div className={styles.main}><TabProvider>
-    </TabProvider></div>;
+  return (
+    <div className={styles.main}>
+      <TabProvider>
+        <Tab />
+      </TabProvider>
+    </div>
+  );
 }
---
+++ src/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/Tab.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./tab.module.css";
+
+export default function Tab() {
+  const [tab, setTab] = useState("rec");
+
+  const onClickRec = () => {
+    setTab("rec");
+  };
+
+  const onClickFol = () => {
+    setTab("fol");
+  };
+
+  return (
+    <div className={styles.tabFixed}>
+      <div className={styles.title}>홈</div>
+      <div className={styles.tab}>
+        <div onClick={onClickRec}>
+          추천
+          <div className={styles.tabIndicator} hidden={tab === "fol"}></div>
+        </div>
+        <div onClick={onClickFol}>
+          팔로우
+          <div className={styles.tabIndicator} hidden={tab === "rec"}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
