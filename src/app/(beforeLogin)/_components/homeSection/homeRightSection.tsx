cd927d930e1996959def84e292ed9d13f3942162
watching-dev <coderefactoring90@gmail.com>
2024-10-28 19:28:16 +0900

    [fix] profile일때 rightSection 수정

--- src/app/(beforeLogin)/_components/homeSection/homeRightSection.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeRightSection.tsx
@@ -1,21 +1,30 @@
+"use client";
+
 import { ReactNode } from "react";
 import RightSectionMain from "../../page";
 import styles from "./homeRightSection.module.css";
 import RightSectionRecommend from "./homeRightSection/RightSectionRecommend";
 import RightSectionSearch from "./homeRightSection/RightSectionSearch";
+import { usePathname } from "next/navigation";
 
 export default function HomeRightSection({
   children,
 }: {
   children: ReactNode;
 }) {
+  const pathName = usePathname();
+
   return (
     <div className={styles.rightSectionWrapper}>
       <div className={styles.rightSectionInner}>
         <div className={styles.mainSection}>{children}</div>
         <div className={styles.rightSection}>
-          <RightSectionSearch />
-          <RightSectionRecommend />
+          {pathName === "/profile" ? null : (
+            <>
+              <RightSectionSearch />
+              <RightSectionRecommend />
+            </>
+          )}
         </div>
       </div>
     </div>
