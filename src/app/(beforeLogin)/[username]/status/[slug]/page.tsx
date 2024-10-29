fff9b22a478d9494d557dfb58d4cc8725c5ad7ed
watching-dev <coderefactoring90@gmail.com>
2024-10-29 23:07:03 +0900

    [feat] 게시글 보기 추가

--- src/app/(beforeLogin)/[username]/status/[slug]/page.tsx
+++ src/app/(beforeLogin)/[username]/status/[slug]/page.tsx
@@ -1,3 +1,14 @@
+import Post from "@/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/Post";
+
+import styles from "./singlePost.module.css";
+
 export default function SinglePost() {
-  return <div>single post</div>;
+  return (
+    <>
+      <div className={styles.tabFixed}>
+        <div className={styles.title}>게시글</div>
+        <Post />
+      </div>
+    </>
+  );
 }
