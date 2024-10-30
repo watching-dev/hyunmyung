2f85b121ccb60276ecd517013f22bead3cf1fbe0
watching-dev <coderefactoring90@gmail.com>
2024-10-30 14:17:18 +0900

    [feat] login modal을 띄우기 위해서 패러렐 라우트 사용

---
+++ src/app/(beforeLogin)/@modal/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginModal() {
+  return <div>login modal</div>;
+}
--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -6,15 +6,20 @@ import HomeRightSection from "./_components/homeSection/HomeRightSection";
 
 export default function BeforeLoginLayout({
   children,
+  modal,
 }: {
   children: ReactNode;
+  modal: ReactNode;
 }) {
   return (
     <>
       {/* <div>before login layout</div> */}
       <div className={styles.container}>
         <HomeLeftSection />
-        <HomeRightSection>{children}</HomeRightSection>
+        <HomeRightSection>
+          {children}
+          {modal}
+        </HomeRightSection>
       </div>
     </>
   );
