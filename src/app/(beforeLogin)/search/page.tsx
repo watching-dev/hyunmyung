44746b12335bd416a9aef2286464f2014d25ef64
watching-dev <coderefactoring90@gmail.com>
2024-10-31 16:34:17 +0900

    [feat] search 추가

---
+++ src/app/(beforeLogin)/search/page.tsx
@@ -0,0 +1,27 @@
+import style from "./search.module.css";
+import RightSectionSearch from "../_components/homeSection/homeRightSection/RightSectionSearch";
+import BackButton from "../_components/homeSection/homeMainSection/tab/BackButton";
+import SearchResult from "./_component/SearchResult";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+export default function Search({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <RightSectionSearch q={searchParams.q} />
+          </div>
+        </div>
+      </div>
+      <div className={style.list}>
+        <SearchResult searchParams={searchParams} />
+      </div>
+    </main>
+  );
+}
