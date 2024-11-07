118d30a6631ed3642622647971eea57dccb5c989
watching-dev <coderefactoring90@gmail.com>
2024-11-07 19:32:08 +0900

    [feat] 한국 현재 시간으로 저장되도록 추가

--- src/app/api/user/join/route.ts
+++ src/app/api/user/join/route.ts
@@ -1,6 +1,6 @@
 import dbConnect from "@/app/_lib/dbConnect";
 import JoinAPIS from "@/app/model/join";
-import * as bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
@@ -25,12 +25,19 @@ export async function POST(req: Request, res: Response) {
     // const userJ = JoinAPIS2;
     // console.log(userJ);
 
-    const userr = new JoinAPIS({
+    const current = new Date();
+    const utc = current.getTime() + current.getTimezoneOffset() * 60 * 1000;
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    // const kr_current = new Date(utc + KR_TIME_DIFF) === Date.now() 똑같음_9시간 부족하니까 2배 해줘야 맞음;
+    const kr_current = new Date(utc + KR_TIME_DIFF * 2);
+    console.log("time:", kr_current);
+
+    const user = new JoinAPIS({
       userId: data.userId,
       userPw: hashPassword,
       name: data.name,
-      createdAt: Date.now(),
-    }); // 모델을 생성하고 나서 여기에서 뭐라도 수정하고 저장 -> post 하면 중복 생성하라고 에러 뜸, 서버 재시작 해야함
+      createdAt: kr_current,
+    }); // 모델을 생성하고 나서 여기에서 뭐라도 수정하고 저장 -> post 하면 중복 생성했다고  에러 뜸, 서버 재시작 해야함
     // 다른 에러거나 unique 설정했는데 안먹는다면 디비 컬렉션 삭제후 다시 생성
     // const u = await JoinAPIS2.create({
     //   userId: 23,
@@ -38,8 +45,8 @@ export async function POST(req: Request, res: Response) {
     //   name: "name",
     //   createdAt: Date.now(),
     // });
-    console.log(userr);
-    await userr.save();
+    console.log(user);
+    await user.save();
     // const userInfo = new UserJoin({
     //   userId: data.userId,
     //   userPw: hashPassword,
