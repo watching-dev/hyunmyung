b2fdc67ed8c4951a153154a80ab9d430401c58fc
watching-dev <coderefactoring90@gmail.com>
2024-11-07 23:40:32 +0900

    [feat] posting 추가

---
+++ src/app/(beforeLogin)/posting/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./posting.module.css";
+
+export default function Posting() {
+  return (
+    <div className={styles.tabFixed}>
+      <div className={styles.title}>작성하기</div>
+    </div>
+  );
+}
