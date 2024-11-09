866b60842b69185aa2d86e9beb700b3cc7e9b62a
watching-dev <coderefactoring90@gmail.com>
2024-11-09 23:51:23 +0900

    [feat] posting before -> after login 으로 이동 및 백버튼 추가

--- src/app/(afterLogin)/posting/page.tsx
+++ src/app/(afterLogin)/posting/page.tsx
@@ -1,8 +1,10 @@
+import BackButton from "@/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/BackButton";
 import styles from "./posting.module.css";
 
 export default function Posting() {
   return (
     <div className={styles.tabFixed}>
+      <BackButton />
       <div className={styles.title}>작성하기</div>
     </div>
   );
