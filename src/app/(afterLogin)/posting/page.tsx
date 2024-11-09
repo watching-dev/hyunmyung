03a3d756b69a00aac40ba5db8b6f97e5ba6b4053
watching-dev <coderefactoring90@gmail.com>
2024-11-09 23:58:29 +0900

    [feat] react-quill 에디터 추가 및 로그인에서만 동작하도록 설정_npm install react-quill --save

--- src/app/(afterLogin)/posting/page.tsx
+++ src/app/(afterLogin)/posting/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import BackButton from "@/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/BackButton";
 import styles from "./posting.module.css";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function Posting() {
+  const [value, setValue] = useState("");
   return (
-    <div className={styles.tabFixed}>
-      <BackButton />
-      <div className={styles.title}>작성하기</div>
-    </div>
+    <>
+      <div className={styles.tabFixed}>
+        <BackButton />
+        <div className={styles.title}>작성하기</div>
+      </div>
+      <ReactQuill
+        className={styles.editor}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
+    </>
   );
 }
