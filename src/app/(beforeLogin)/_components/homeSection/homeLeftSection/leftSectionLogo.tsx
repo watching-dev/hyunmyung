c82cce9dde702f52eb0689379f6e5191108cdf09
watching-dev <coderefactoring90@gmail.com>
2024-11-12 19:12:35 +0900

    [fix] logo 이미지로 수정

--- src/app/(beforeLogin)/_components/homeSection/homeLeftSection/leftSectionLogo.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeLeftSection/leftSectionLogo.tsx
@@ -6,7 +6,13 @@ export default async function LeftSectionLogo() {
   const session = await auth();
   return (
     <div className={styles.bg}>
-      <div className={styles.logo}>로고</div>
+      <div className={styles.logo}>
+        <svg viewBox="0 0 24 24" aria-hidden="true">
+          <g>
+            <path d="M7.323 2h11.443l-3 5h6.648L6.586 22.83 7.847 14H2.523l4.8-12zm1.354 2l-3.2 8h4.676l-.739 5.17L17.586 9h-5.352l3-5H8.677z"></path>
+          </g>
+        </svg>
+      </div>
       <LogoutButton me={session} />
     </div>
   );
