9e6a8cf049163bb7201cddb15a730e052674dea8
watching-dev <coderefactoring90@gmail.com>
2024-11-13 17:27:09 +0900

    [fix] 시간은 제대로 저장 됐는데 왜 출력은 9시간 차이나지.. 수정함

--- src/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/Post.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/Post.tsx
@@ -8,9 +8,37 @@ import Link from "next/link";
 import { Post as IPost } from "@/app/model/Post";
 import Image from "next/image";
 import { IList } from "@/app/api/posts/route";
+import updateLocale from "dayjs/plugin/updateLocale";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: (value: string) => {
+      if (value === "0 seconds") {
+        return "Just Now";
+      }
+
+      if (value === "1 seconds") {
+        return "1 second ago";
+      }
+      return `${value} ago`;
+    },
+    s: "%d seconds",
+    m: "a minute",
+    mm: "%d minutes",
+    h: "an hour",
+    hh: "%d hours",
+    d: "a day",
+    dd: "%d days",
+    M: "a month",
+    MM: "%d months",
+    y: "a year",
+    yy: "%d years",
+  },
+});
 
 type Props = {
   // post: IPost;
@@ -19,6 +47,17 @@ type Props = {
 
 export default function Post({ post }: Props) {
   const data = post;
+  console.log("posttt", data);
+  console.log("created", data.createdAt);
+  console.log("dayjs", dayjs(Date.now()).from(data.createdAt));
+  console.log("dayjs now", dayjs(data.createdAt).fromNow(true));
+  console.log(
+    "diff",
+    dayjs(dayjs(data.createdAt).subtract(9, "hour").format()).fromNow(true)
+  );
+  const time = dayjs(
+    dayjs(data.createdAt).subtract(9, "hour").format()
+  ).fromNow(true);
 
   const noImage: boolean = true; // data.Images[0].link === "" ? true : false;
   // if (Math.random() > 0.5) {
@@ -42,9 +81,7 @@ export default function Post({ post }: Props) {
             &nbsp;
             <div className={styles.postUserId}>{data.Profile.description}</div>
             &nbsp; · &nbsp;
-            <div className={styles.postDate}>
-              {dayjs(data.createdAt).fromNow(true)}
-            </div>
+            <div className={styles.postDate}>{time}</div>
           </div>
           {/* <div>{data.content}</div> */}
           <div className={styles.titleImage}>
