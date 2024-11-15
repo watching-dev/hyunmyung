b2a698646e23feeac7ce0c4e9091ff8d182d781a
watching-dev <coderefactoring90@gmail.com>
2024-11-15 11:53:36 +0900

    [fix] post 들어가니까 에러남, postId가 props.params에서 넘어올때 특수문자가 인코딩 되면서 변해버림, decodeURIComponenet 함수로 디코딩 후 다시 atob() 해서 해결

--- src/app/(beforeLogin)/post/[[...slug]]/page.tsx
+++ src/app/(beforeLogin)/post/[[...slug]]/page.tsx
@@ -31,10 +31,17 @@ type Props = {
 };
 export default async function PostSlug(props: any) {
   try {
+    // props.params.slug[0](postId)가 인코딩 된 값이 MTFfY1hkbGNnPT1fMQ== 인데 특수문자 인코딩문제로 MTFfY1hkbGNnPT1fMQ%3D%3D 이렇게 변환되어 넘어옴
+    // 그래서 atob() 할때 에러나서 DOMException [InvalidCharacterError]: Invalid character 뜸
+    // 디코딩을 먼저 다시 하고 변환으로 해결
+    const decodeURI = decodeURIComponent(props.params.slug[0]);
+    const decodeSlug = decodeURIComponent(props.params.slug[1]);
+    console.log("decode:", decodeURI);
+    console.log("decode slug", decodeSlug);
     console.log("props slug", props.params.slug); // 이렇게 해야 url 값을 가져오네
-    const originPostId = atob(props.params.slug[0]); // 디코딩이 실패하면 에러남_인코딩 글자를 임의로 수정했을 경우처럼
+    const originPostId = atob(decodeURI); // 디코딩이 실패하면 에러남_인코딩 글자를 임의로 수정했을 경우처럼
     console.log("origin:", originPostId);
-    const slug = props.params.slug[1].replace(/-/gi, " "); // -를 공백으로 치환
+    const slug = decodeSlug.replace(/-/gi, " "); // -를 공백으로 치환
     console.log("slug==", slug);
 
     const response = await fetch(
