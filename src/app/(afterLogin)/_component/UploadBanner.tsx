b6c7eb2dd29143f864f139c424a57229629e29c2
watching-dev <coderefactoring90@gmail.com>
2024-11-21 10:46:28 +0900

    [chore] uploadBanner 코드정리_문제 해결_타입 매칭_주석제거

--- src/app/(afterLogin)/_component/UploadBanner.tsx
+++ src/app/(afterLogin)/_component/UploadBanner.tsx
@@ -5,51 +5,59 @@ import styles from "./uploadBanner.module.css";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import imageCompression from "browser-image-compression";
 import { storage } from "@/firebase/config";
+import { useRouter } from "next/navigation";
 
 export default function UploadBanner() {
+  const [banner, setBanner] = useState<File>();
+  const [preview, setPreview] = useState<string>();
+  const router = useRouter();
+
   const uploadBanner = async () => {
-    try {
-      console.log("upload==", banner);
-      const fileName = `${Date.now().toString()}_${banner.name}`;
-      console.log("filename==: ", fileName);
-      const storageRef = ref(storage, `images/banner/${fileName}`);
-      console.log("storageRef", storageRef);
-      const imageFile = new File([banner], fileName, { type: "image/jpeg" });
-      console.log("imageFile", imageFile);
-      const options = {
-        maxSizeMB: 1,
-        maxWidthOrHeight: 1920,
-        useWebWorker: true,
-      };
-      const compressdFile = await imageCompression(imageFile, options);
-      console.log("compressedFile", compressdFile);
-      const snapshot = await uploadBytes(storageRef, compressdFile);
-      console.log("snapshot", snapshot);
-      // const snapshot = await uploadBytes(storageRef, compressdFile);
-      const url = await getDownloadURL(snapshot.ref);
-      console.log("url:", url);
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE}/api/banner`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ url }),
-        }
-      );
-      console.log(response);
-      const res = await response.json(); // 이렇게 해야 내가 원하는 response를 받을수 있구나
-      console.log(res);
-      // router.replace("/");
-    } catch (error) {
-      console.error(error);
-      alert("업로드 or db 에러");
+    if (banner !== null || banner !== undefined) {
+      try {
+        console.log("upload==", banner);
+        const fileName = `${Date.now().toString()}_${banner!.name}`;
+        console.log("filename==: ", fileName);
+        const storageRef = ref(storage, `images/banner/${fileName}`);
+        console.log("storageRef", storageRef);
+        const imageFile = new File([banner!], fileName, { type: "image/jpeg" });
+        console.log("imageFile", imageFile);
+        const options = {
+          maxSizeMB: 1,
+          maxWidthOrHeight: 1920,
+          useWebWorker: true,
+        };
+        const compressdFile = await imageCompression(imageFile, options);
+        console.log("compressedFile", compressdFile);
+        const snapshot = await uploadBytes(storageRef, compressdFile);
+        console.log("snapshot", snapshot);
+        const url = await getDownloadURL(snapshot.ref);
+        console.log("url:", url);
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE}/api/banner`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url }),
+          }
+        );
+        console.log(response);
+        const res = await response.json(); // await 해야 데이터 제대로 확인 가능
+        console.log(res);
+        router.replace("/");
+      } catch (error) {
+        console.error(error);
+        alert(`업로드 에러: ${error}`);
+      }
+    } else {
+      console.log("banner === null || undefined");
     }
   };
 
   // 파일의 url 가져옴, input의 onChange에 실행
-  const readURL = (e) => {
+  const readURL = (e: any) => {
     // 업로드한 파일이 있는지 체크
     if (e.target.files.length) {
       // 있으면 FileReader 객체 생성
@@ -61,7 +69,7 @@ export default function UploadBanner() {
       setBanner(e.target.files[0]);
 
       // 읽기 동작이 성공적으로 load됐을때 발생할 이벤트 핸들러
-      reader.onload = function (e) {
+      reader.onload = function (e: any) {
         // state에 담아줌
         // setPreviewImg(e.target.result);
         console.log(e.target.result);
@@ -69,8 +77,6 @@ export default function UploadBanner() {
       };
     }
   };
-  const [banner, setBanner] = useState(null);
-  const [preview, setPreview] = useState(null);
 
   return (
     <div>
@@ -86,7 +92,7 @@ export default function UploadBanner() {
           />
         </div>
 
-        {preview === null ? (
+        {preview === null || preview === undefined || preview === "" ? (
           <></>
         ) : (
           <>
