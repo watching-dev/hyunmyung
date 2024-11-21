3659d3d038aab5b614566e7508cd871504f7885c
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:58:28 +0900

    [fix] 브라우저에서 svg 에러나는 문제 수정

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -33,9 +33,9 @@ export default function LogoutButton({ me }: Props) {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
             <polyline points="16 17 21 12 16 7" />
