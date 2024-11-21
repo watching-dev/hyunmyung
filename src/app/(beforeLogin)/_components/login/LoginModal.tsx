6747e1af3f6a94d9243e2d4908a6b566091de79a
watching-dev <coderefactoring90@gmail.com>
2024-11-21 14:42:18 +0900

    [chore] loginModal 코드정리_주석제거

--- src/app/(beforeLogin)/_components/login/LoginModal.tsx
+++ src/app/(beforeLogin)/_components/login/LoginModal.tsx
@@ -34,6 +34,8 @@ export default function LoginModal() {
       // response?.error는 문자열이니까 무조건 true로 떨어지지..
       // 통신 자체는 성공 한거고(status 200, ok true) 그 안에서 이름, 비번이 틀린걸 검증했어야 했던 것
       // 서버에서 날라오는게 이상하게 되어 있어서 그런거.. error가 왜 저따구로 와서 햇갈리게 하냐
+
+      // ====>>>> fetch api 통일성 있게 템플릭화 필요
       if (
         response?.code === "500" ||
         (response?.code === null && response?.url === null)
@@ -43,15 +45,6 @@ export default function LoginModal() {
         router.replace("/");
         router.refresh();
       }
-
-      // response 찍어보면 아래처럼 나온다
-      //   {
-      //     "error": "CredentialsSignin",
-      //     "code": "no_user",
-      //     "status": 200,
-      //     "ok": true,
-      //     "url": null
-      // }
     } catch (error) {
       setMessage("에러");
     }
