551f3e5e134f076e01c84d687dd99d99e767c7b5
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:42:38 +0900

    [chore] api search console.log 삭제

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,25 +4,25 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
-    console.log("req", req.url);
+    // console.log("req", req.url);
     await dbConnect();
     const { searchParams } = new URL(req.url);
     const q = searchParams.get("q");
-    console.log("q", q);
+    // console.log("q", q);
     const postList = await PostingAPIS.find().or([
       { content: { $regex: q } },
       { title: { $regex: q } },
     ]);
 
-    console.log("q length:", q?.length);
+    // console.log("q length:", q?.length);
 
     // if (q === "p") {
     if (q?.length === 1) {
-      console.log("search is p:", q);
+      // console.log("search is p:", q);
       const postList = await PostingAPIS.find({ title: { $regex: q } });
       return NextResponse.json(postList.reverse());
     }
-    console.log("search list", postList);
+    // console.log("search list", postList);
     return NextResponse.json(postList.reverse());
   } catch (err) {
     console.error(err);
