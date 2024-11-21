f9bf559f7f76ba52e1c8badf2d39b79041706f93
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:41:06 +0900

    [chore] api profile id console.log 삭제

--- src/app/api/profile/id/route.ts
+++ src/app/api/profile/id/route.ts
@@ -7,12 +7,12 @@ export async function POST(req: Request, res: Response) {
 
     // 입력한 데이터
     const data = await req.json();
-    console.log("data==>>", data);
+    // console.log("data==>>", data);
 
     const profileData = await ProfileAPIS.findOne({
       "User.userId": data,
     }).sort({ createdAt: -1 });
-    console.log("profileData", profileData);
+    // console.log("profileData", profileData);
 
     return Response.json(profileData);
   } catch (error) {
