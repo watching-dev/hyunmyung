eabea684ca18c2e3a54866983c5d46cdac846b99
watching-dev <coderefactoring90@gmail.com>
2024-11-21 12:43:43 +0900

    [chore] getPosts 코드정리_주석제거

--- src/app/(afterLogin)/_lib/getPosts.ts
+++ src/app/(afterLogin)/_lib/getPosts.ts
@@ -4,21 +4,20 @@ export interface IPage {
 export async function getPosts(pageParam: IPage) {
   console.log("IPAGE=====", pageParam);
   const res = await fetch(
-    // `http://localhost:9090/api/posts/r  ecommends?cursor=${pageParam}`,
-    // `http://localhost:3000/api/posts?cursor=${pageParam}`,
-    `http://localhost:3000/api/infinitePost?page=${pageParam}`,
+    `${process.env.NEXT_PUBLIC_BASE}/api/infinitePost?page=${pageParam}`,
     {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
       next: {
         tags: ["posts", "all"],
         // revalidate: 30,
       },
     }
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
