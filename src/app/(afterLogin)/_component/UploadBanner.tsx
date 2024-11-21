c125c962d74fe25ee5126cd461db60e8531feaa6
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:13:06 +0900

    [chore] uploadBanner console.log 삭제

--- src/app/(afterLogin)/_component/UploadBanner.tsx
+++ src/app/(afterLogin)/_component/UploadBanner.tsx
@@ -19,45 +19,38 @@ export default function UploadBanner() {
   // ----> 서버가 아니라 브라우저에서 시간 계산이 돼서 그런지 9시간 더해져서 나옴, 2배 -> 1배로 변경
   // 배포 후에는 각각 시간 어떻게 적용되는지 파악해야 할듯, utc로 하든지 등등
   const kr_current = new Date(utc + KR_TIME_DIFF);
-  console.log(
-    "kr",
-    kr_current,
-    "year",
-    kr_current.getFullYear(),
-    "mon",
-    kr_current.getMonth() + 1, // month는 0부터 시작하기 때문에 1 더해줘야 함
-    "next",
-    kr_current.getMonth() + 2,
-    "day",
-    kr_current.getDate()
-  );
+  // console.log(
+  //   "kr",
+  //   kr_current,
+  //   "year",
+  //   kr_current.getFullYear(),
+  //   "mon",
+  //   kr_current.getMonth() + 1, // month는 0부터 시작하기 때문에 1 더해줘야 함
+  //   "next",
+  //   kr_current.getMonth() + 2,
+  //   "day",
+  //   kr_current.getDate()
+  // );
 
   const uploadBanner = async () => {
     if (banner !== null || banner !== undefined) {
       try {
-        console.log("upload==", banner);
         const fileName = `${Date.now().toString()}_${banner!.name}`;
-        console.log("filename==: ", fileName);
         const storageRef = ref(
           storage,
           `images/banner/${kr_current.getFullYear()}/${
             kr_current.getMonth() + 1
           }/${kr_current.getDate()}/${fileName}`
         );
-        console.log("storageRef", storageRef);
         const imageFile = new File([banner!], fileName, { type: "image/jpeg" });
-        console.log("imageFile", imageFile);
         const options = {
           maxSizeMB: 1,
           maxWidthOrHeight: 1920,
           useWebWorker: true,
         };
         const compressdFile = await imageCompression(imageFile, options);
-        console.log("compressedFile", compressdFile);
         const snapshot = await uploadBytes(storageRef, compressdFile);
-        console.log("snapshot", snapshot);
         const url = await getDownloadURL(snapshot.ref);
-        console.log("url:", url);
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_BASE}/api/banner`,
           {
@@ -68,16 +61,14 @@ export default function UploadBanner() {
             body: JSON.stringify({ url }),
           }
         );
-        console.log(response);
         const res = await response.json(); // await 해야 데이터 제대로 확인 가능
-        console.log(res);
         router.replace("/");
       } catch (error) {
         console.error(error);
         alert(`업로드 에러: ${error}`);
       }
     } else {
-      console.log("banner === null || undefined");
+      // console.log("banner === null || undefined");
     }
   };
 
@@ -89,15 +80,13 @@ export default function UploadBanner() {
       const reader = new FileReader();
       // reader에게 파일 url 읽으라고 함
       reader.readAsDataURL(e.target.files[0]);
-      console.log(e.target.files.length);
-      console.log(e.target.files[0]);
+      // console.log(e.target.files.length);
+      // console.log(e.target.files[0]);
       setBanner(e.target.files[0]);
 
       // 읽기 동작이 성공적으로 load됐을때 발생할 이벤트 핸들러
       reader.onload = function (e: any) {
         // state에 담아줌
-        // setPreviewImg(e.target.result);
-        console.log(e.target.result);
         setPreview(e.target.result);
       };
     }
