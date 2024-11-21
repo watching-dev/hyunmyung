e20e06e2e247859f4e7cae95a53b36ed1dec512e
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:39:26 +0900

    [chore] api post detail console.log 삭제

--- src/app/api/post/detail/route.ts
+++ src/app/api/post/detail/route.ts
@@ -7,13 +7,13 @@ export async function POST(req: Request, res: Response) {
 
     // 입력한 데이터
     const data = await req.json();
-    console.log("data==>>", data);
+    // console.log("data==>>", data);
 
     const postedData = await PostingAPIS.findOne({
       postId: data.originPostId,
       title: data.slug,
     });
-    console.log("postedData", postedData);
+    // console.log("postedData", postedData);
     return Response.json(postedData);
   } catch (error) {
     console.log("err===>", error);
