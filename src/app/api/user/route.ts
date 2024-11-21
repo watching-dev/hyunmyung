09df0b8f82689e8cfbef0769c3f59260f993aeac
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:44:14 +0900

    [chore] api user console.log 삭제

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,14 +5,14 @@ export async function POST(req: Request, res: Response) {
     await dbConnect();
 
     const data = await req.json();
-    console.log(data);
+    // console.log(data);
 
     const current = new Date();
     const utc = current.getTime() + current.getTimezoneOffset() * 60 * 1000;
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
     // const kr_current = new Date(utc + KR_TIME_DIFF) === Date.now() 똑같음_9시간 부족하니까 2배 해줘야 맞음;
     const kr_current = new Date(utc + KR_TIME_DIFF * 2);
-    console.log("time:", kr_current);
+    // console.log("time:", kr_current);
 
     // const user = new UserAPIS({
     //   userId: data.userId,
