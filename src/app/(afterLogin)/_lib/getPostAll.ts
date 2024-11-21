1bd9c7c2c5883d06d80f21c4c2808ee5097915ac
watching-dev <coderefactoring90@gmail.com>
2024-11-21 12:35:35 +0900

    [chore] getPostAll 코드정리_주석제거

--- src/app/(afterLogin)/_lib/getPostAll.ts
+++ src/app/(afterLogin)/_lib/getPostAll.ts
@@ -1,21 +1,16 @@
-// type Props = { pageParam?: number };
 export async function getPostAll() {
-  const res = await fetch(
-    // `http://localhost:9090/api/posts/r  ecommends?cursor=${pageParam}`,
-    // `http://localhost:3000/api/posts?cursor=${pageParam}`,
-    `http://localhost:3000/api/posts`,
-    {
-      next: {
-        tags: ["posts", "all"],
-        // revalidate: 30,
-      },
-    }
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE}/api/posts`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    next: {
+      tags: ["posts", "all"],
+      // revalidate: 30,
+    },
+  });
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
