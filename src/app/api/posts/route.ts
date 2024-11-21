1196177a72b475a8bfc0676e45173061f3ec30db
watching-dev <coderefactoring90@gmail.com>
2024-11-21 15:15:08 +0900

    [chore] api posts 코드정리_주석제거

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,5 @@
 import dbConnect from "@/app/_lib/dbConnect";
 import PostingAPIS from "@/app/model/posting";
-import PostsAPI from "@/app/model/Posts";
-import { StringModule } from "@faker-js/faker";
 import { NextResponse } from "next/server";
 
 export interface IList {
@@ -23,22 +21,8 @@ export interface IList {
 export async function GET(req: Request) {
   try {
     await dbConnect();
-    const allPost = await PostingAPIS.find(/*{ sort: { createdAt: -1 } }*/);
-    // const { searchParams } = new URL(req.url);
-    // const page = Number(searchParams.get("cursor")) || 0;
-    // console.log("sp", searchParams);
-    // console.log("page", page);
-    // console.log("req.url", req.url);
-    // console.log("list", postList);
-    // const cursorList = postList[page - 1];
-    // if (page === 0) {
-    //   return NextResponse.json(postList);
-    // }
-    // if (page > postList.length) {
-    //   return NextResponse.json(null);
-    // } else {
-    //   return NextResponse.json(cursorList);
-    // }
+    const allPost = await PostingAPIS.find();
+
     console.log("posting:", allPost);
 
     const postList = allPost.reverse().map((post) => {
