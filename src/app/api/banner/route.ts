6546fbbc26d860364d3eed43270d7ca43c475259
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:38:10 +0900

    [chore] api banner console.log 삭제

--- src/app/api/banner/route.ts
+++ src/app/api/banner/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: Request) {
     await dbConnect();
     const banner = await BannerAPIS.findOne().sort({ createdAt: -1 });
     // 1: 가장 오래, -1: 가장 최신
-    console.log(banner);
+    // console.log(banner);
     return NextResponse.json(banner);
   } catch (err) {
     console.error(err);
@@ -20,21 +20,21 @@ export async function POST(req: Request, res: Response) {
     await dbConnect();
 
     const data = await req.json();
-    console.log(data);
+    // console.log(data);
 
     const current = new Date();
     const utc = current.getTime() + current.getTimezoneOffset() * 60 * 1000;
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
     // const kr_current = new Date(utc + KR_TIME_DIFF) === Date.now() 똑같음_9시간 부족하니까 2배 해줘야 맞음;
     const kr_current = new Date(utc + KR_TIME_DIFF * 2);
-    console.log("time:", kr_current);
+    // console.log("time:", kr_current);
 
     const banner = new BannerAPIS({
       bannerURL: data.url,
       createdAt: kr_current,
       updatedAt: kr_current,
     });
-    console.log(banner);
+    // console.log(banner);
     await banner.save();
 
     return Response.json({ message: "Save Success" });
