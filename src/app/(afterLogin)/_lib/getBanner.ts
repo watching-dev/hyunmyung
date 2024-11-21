2f8cb8e3e5b10a1f78ebd58794311948750955e9
watching-dev <coderefactoring90@gmail.com>
2024-11-21 10:52:46 +0900

    [chore] getBanner 코드정리_주석제거

--- src/app/(afterLogin)/_lib/getBanner.ts
+++ src/app/(afterLogin)/_lib/getBanner.ts
@@ -1,20 +1,15 @@
-// type Props = { pageParam?: number };
 export async function getBanner() {
-  const res = await fetch(`http://localhost:3000/api/banner`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE}/api/banner`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
     next: {
       tags: ["posts", "banner"],
     },
   });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // const resJson = await res.json(); // 데이터를 가져오고 싶으면 꼭 await 붙여라
-  // console.log("resJson", resJson);
-  // const { bannerURL } = resJson;
-  // console.log("banner==", bannerURL);
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
