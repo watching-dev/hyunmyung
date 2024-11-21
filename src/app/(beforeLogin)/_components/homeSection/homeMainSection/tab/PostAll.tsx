cf7a54844a19434cc15456c274e2f4954abad769
watching-dev <coderefactoring90@gmail.com>
2024-11-21 13:35:56 +0900

    [chore] postAll 코드정리_문제 해결_타입 매칭_주석제거

--- src/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/PostAll.tsx
+++ src/app/(beforeLogin)/_components/homeSection/homeMainSection/tab/PostAll.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import {
-  InfiniteData,
-  useInfiniteQuery,
-  useQuery,
-} from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import Post from "./Post";
-import { Post as IPost } from "@/app/model/Post";
-import { getPostAll } from "@/app/(afterLogin)/_lib/getPostAll";
 import { Fragment, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { IList } from "@/app/api/posts/route";
@@ -15,70 +9,9 @@ import { getPosts, IPage } from "@/app/(afterLogin)/_lib/getPosts";
 import { HashLoader } from "react-spinners";
 
 export default function PostAll() {
-  // const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
-  //   IPost[],
-  //   Object,
-  //   InfiniteData<IPost[]>,
-  //   [_1: string, _2: string],
-  //   number
-  // >({
-  //   queryKey: ["posts", "recommends"],
-  //   queryFn: getPostRecommends,
-  //   initialPageParam: 1,
-  //   getNextPageParam: (lastPage, allpages) => {
-  //     // lastPage.at(-1)?.postId;
-
-  //     console.log("last", lastPage.at(-1));
-  //     console.log("all", allpages);
-  //     const nextPage = lastPage.length + 1;
-  //     // return lastPage.length === 0 ? undefined : nextPage;
-  //     // return undefined;
-  //     return lastPage.at(-1)?.postId;
-  //   },
-  //   staleTime: 60 * 1000,
-  // });
-  // console.log("daata", data);
-  // console.log("isfetching", isFetching);
-  // console.log("hasnextpage", hasNextPage);
-  // console.log("fetchNextPage", fetchNextPage);
-
-  // const { ref, inView } = useInView({ threshold: 0, delay: 0 });
-
-  // useEffect(() => {
-  //   if (inView) {
-  //     !isFetching && hasNextPage && fetchNextPage();
-  //   }
-  // }, [inView, isFetching, hasNextPage, fetchNextPage]);
-  // console.log(data);
-  // return (
-  //   <>
-  //     {
-  //       data?.pages.map((page, i) => (
-  //         <Fragment key={i}>
-  //           {page.map((post) => (
-  //             <Post key={post.postId} post={post} />
-  //           ))}
-  //         </Fragment>
-  //       ))
-  //       //   data?.pages.map((post) => (
-  //       //     <Post key={post.postId} post={post} />
-  //       //   ))
-  //     }
-  //     <div ref={ref} style={{ height: 50 }} />
-  //   </>
-  // );
-  // const { data } = useQuery<IList[]>({
-  //   queryKey: ["posts", "all"],
-  //   queryFn: getPostAll,
-  //   staleTime: 60 * 1000,
-  // });
-
   const { ref, inView } = useInView({ threshold: 0, delay: 0 });
   const { data, fetchNextPage, isPending, isFetching, isLoading } =
-    useInfiniteQuery<
-      IList[]
-      // InfiniteData<IList[]>
-    >({
+    useInfiniteQuery<IList[]>({
       queryKey: ["posts", "all"],
       queryFn: async ({ pageParam = 1 }: IPage) => await getPosts(pageParam),
       initialPageParam: 1,
@@ -96,9 +29,6 @@ export default function PostAll() {
   }, [inView, fetchNextPage]);
 
   console.log("qq==>", data?.pages.flat());
-  // console.log("length", data?.pages?.[0]?.length);
-  // return data?.map((post) => <Post key={post.postId} post={post} />);
-  // (data === undefined || data === null ? (return null) : (return <Post post={data} />));
   console.log(
     "before",
     "isLoading",
