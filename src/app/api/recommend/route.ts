5cb9282e256caf031637e2945bb2df8142799dc5
watching-dev <coderefactoring90@gmail.com>
2024-11-21 18:42:13 +0900

    [chore] api recommend console.log 삭제

--- src/app/api/recommend/route.ts
+++ src/app/api/recommend/route.ts
@@ -16,40 +16,40 @@ export async function GET() {
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
     // const kr_current = new Date(utc + KR_TIME_DIFF) === Date.now() 똑같음_9시간 부족하니까 2배 해줘야 맞음;
     const kr_current = new Date(utc + KR_TIME_DIFF * 2);
-    console.log(
-      "kr",
-      kr_current,
-      "year",
-      kr_current.getFullYear(),
-      "mon",
-      kr_current.getMonth() + 1, // month는 0부터 시작하기 때문에 1 더해줘야 함
-      "next",
-      kr_current.getMonth() + 2
-    );
-    console.log("------------");
-    console.log(
-      "gte:",
-      `${kr_current.getFullYear()}-${kr_current.getMonth() + 1}-01`
-    );
-    console.log(
-      "lt:",
-      `${kr_current.getFullYear()}-${kr_current.getMonth() + 2}-01`
-    );
+    // console.log(
+    //   "kr",
+    //   kr_current,
+    //   "year",
+    //   kr_current.getFullYear(),
+    //   "mon",
+    //   kr_current.getMonth() + 1, // month는 0부터 시작하기 때문에 1 더해줘야 함
+    //   "next",
+    //   kr_current.getMonth() + 2
+    // );
+    // console.log("------------");
+    // console.log(
+    //   "gte:",
+    //   `${kr_current.getFullYear()}-${kr_current.getMonth() + 1}-01`
+    // );
+    // console.log(
+    //   "lt:",
+    //   `${kr_current.getFullYear()}-${kr_current.getMonth() + 2}-01`
+    // );
 
-    console.log(
-      "dayjs",
-      dayjs(
-        `${kr_current.getFullYear()}-${kr_current.getMonth() + 1}-01`
-      ).toDate()
-    );
+    // console.log(
+    //   "dayjs",
+    //   dayjs(
+    //     `${kr_current.getFullYear()}-${kr_current.getMonth() + 1}-01`
+    //   ).toDate()
+    // );
     const allPost = await PostingAPIS.find({
       createdAt: {
         $gte: `${kr_current.getFullYear()}-${kr_current.getMonth() + 1}-01`,
         $lt: `${kr_current.getFullYear()}-${kr_current.getMonth() + 2}-01`,
       },
     }).sort({ createdAt: -1 });
-    console.log(">>>>>>>>");
-    console.log("count:", allPost.length, "allPost", allPost);
+    // console.log(">>>>>>>>");
+    // console.log("count:", allPost.length, "allPost", allPost);
     return NextResponse.json(allPost);
   } catch (error) {
     console.error(error);
