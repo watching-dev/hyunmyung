9e79ad471131779f48dc1ec6e31661b70b2b48b0
watching-dev <coderefactoring90@gmail.com>
2024-11-23 18:18:43 +0900

    [fix] vercel build error 수정_local에서 build는 문제 없는데 왜이러지_Error: Invalid rewrite found

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/upload/:slug",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/upload/:slug",
+  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
+  //     },
+  //   ];
+  // },
 
   images: {
     domains: [
