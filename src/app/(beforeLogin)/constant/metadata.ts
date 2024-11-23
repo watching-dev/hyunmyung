1dc6e97bd0ad2a30b7b91df5300086cbf159ca09
watching-dev <coderefactoring90@gmail.com>
2024-11-23 17:30:54 +0900

    [feat] metadata 추가_일단 동적 말고 정적으로만 적용

---
+++ src/app/(beforeLogin)/constant/metadata.ts
@@ -0,0 +1,109 @@
+import { Metadata } from "next";
+
+export const META = {
+  hyunMyung: "HyunMyung",
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE}`),
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_BASE}`,
+  },
+  title: "Magazine / HyunMyung",
+  description: "HyunMyung 블로그",
+  siteName: "HyunMyung",
+  icons: "../favicon.ico",
+  generator: "Next.js",
+  applicationName: "HyunMyung",
+  referrer: "origin-when-cross-origin",
+  keyword: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "TypeScript",
+    "front-end",
+    "front",
+    "web",
+    "back-end",
+    "developer",
+    "기술블로그",
+    "블로그",
+    "blog",
+    "ios",
+    "react-native",
+  ],
+  authors: { name: "HyunMyung", url: `${process.env.NEXT_PUBLIC_BASE}` },
+  creator: "HyunMyung",
+  publisher: "HyunMyung",
+  openGraph: {
+    title: "Magazine / HyunMyung",
+    description: "HyunMyung 블로그",
+    siteName: "Magazine / HyunMyung",
+    locale: "ko_KR",
+    type: "website",
+    url: `${process.env.NEXT_PUBLIC_BASE}`,
+    // images: {
+    //   url: OG_IMAGE,
+    // },
+  },
+  verification: {
+    // google: META.googleVerification,
+    // other: {
+    //   'naver-site-verification': META.naverVerification,
+    // },
+  },
+  twitter: {
+    title: "Magazine / HyunMyung",
+    description: "HyunMyung 블로그",
+    // images: {
+    //   url: OG_IMAGE,
+    // },
+  },
+  url: `${process.env.NEXT_PUBLIC_BASE}`,
+  type: "website",
+  googleVerification: "xxx",
+  naverVerification: "xxx",
+  ogImage: "/opengraph-image.png",
+} as const;
+
+// export const getMetadata = (metadataProps?: generateMetadataProps) => {
+//   const { title, description, asPath, ogImage } = metadataProps || {};
+
+//   const TITLE = title ? `${title} | 반디부디` : META.title;
+//   const DESCRIPTION = description || META.description;
+//   const PAGE_URL = asPath ? asPath : "";
+//   const OG_IMAGE = ogImage || META.ogImage;
+
+//   const metadata: Metadata = {
+//     metadataBase: new URL(META.url),
+//     alternates: {
+//       canonical: PAGE_URL,
+//     },
+//     title: TITLE,
+//     description: DESCRIPTION,
+//     keywords: [...META.keyword],
+//     openGraph: {
+//       title: TITLE,
+//       description: DESCRIPTION,
+//       siteName: TITLE,
+//       locale: "ko_KR",
+//       type: "website",
+//       url: PAGE_URL,
+//       images: {
+//         url: OG_IMAGE,
+//       },
+//     },
+//     verification: {
+//       google: META.googleVerification,
+//       other: {
+//         "naver-site-verification": META.naverVerification,
+//       },
+//     },
+//     twitter: {
+//       title: TITLE,
+//       description: DESCRIPTION,
+//       images: {
+//         url: OG_IMAGE,
+//       },
+//     },
+//   };
+
+//   return metadata;
+// };
--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -4,6 +4,62 @@ import HomeLeftSection from "./_components/homeSection/HomeLeftSection";
 import styles from "./layout.module.css";
 import HomeRightSection from "./_components/homeSection/HomeRightSection";
 import RQProvider from "../(afterLogin)/_component/RQProvider";
+import { Metadata } from "next";
+import { META } from "./constant/metadata";
+
+export const metadata: Metadata = {
+  metadataBase: META.metadataBase,
+  alternates: {
+    canonical: META.alternates.canonical,
+  },
+  title: {
+    template: `%s / ${META.hyunMyung}`,
+    default: `${META.title}`,
+  },
+  description: META.description,
+  icons: META.icons,
+  generator: META.generator,
+  applicationName: META.applicationName,
+  referrer: META.referrer,
+  keywords: `${META.keyword}`,
+  authors: { name: META.authors.name, url: META.authors.url },
+  creator: META.creator,
+  publisher: META.publisher,
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: {
+      template: `%s :: ${META.hyunMyung}`,
+      default: META.openGraph.title,
+    },
+    description: META.openGraph.description,
+    url: META.openGraph.url,
+    siteName: META.openGraph.siteName,
+    locale: META.openGraph.locale,
+    type: META.openGraph.type,
+    // images: [
+    //   {
+    //     url: 'https://nextjs.org/og.png',
+    //     width: 800,
+    //     height: 600,
+    //   },
+    //   {
+    //     url: 'https://nextjs.org/og-alt.png',
+    //     width: 1800,
+    //     height: 1600,
+    //     alt: 'My custom alt',
+    //   },
+    // ],
+    images: META.ogImage,
+  },
+  twitter: {
+    title: META.twitter.title,
+    description: META.twitter.description,
+  },
+};
 
 export default function BeforeLoginLayout({
   children,
