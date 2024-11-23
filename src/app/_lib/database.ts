c5a3c4bec154403c84697adf784d56165440296c
watching-dev <coderefactoring90@gmail.com>
2024-11-23 11:03:33 +0900

    [fix] build에서 type error - lint 문제로 안되는것 수정

--- src/app/_lib/database.ts
+++ src/app/_lib/database.ts
@@ -8,6 +8,7 @@ if (!url) {
   throw new Error("환경 변수가 설정되지 않았습니다.");
 }
 
+// lint rules에서 no-var를 적용하고 global.d.ts에서 var를 let or const로 바꾸면 아래 _mongo를 찾을수 없다고 나옴
 if (process.env.NODE_ENV === "development") {
   // 개발 중 재실행을 막음
   if (!global._mongo) {
--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,6 +1,7 @@
 import type { MongoClient } from "mongodb";
 import { Mongoose } from "mongoose";
 
+// 아래 변수는 let, const 안됨, var만 가능해서 lint rules 끔
 declare global {
   namespace globalThis {
     var _mongo: Promise<MongoClient>;
